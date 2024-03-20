f1f10f4921d46d78e7cf6fa62ae39af4b477de03
Sharon <104804961+Sharondafrin@users.noreply.github.com>
2024-03-20 18:53:56 +0530

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,12 @@
+
+config = {
+    'PATH': {
+        'LOGGER': r'./logs/template-generation.log',
+        # 'SAVE-DIR': r'./uploads'
+    },
+
+    'SERVICE_MANAGER': {
+        'LOCAL': r'./config/service_manager.ini',
+        'GLOBAL': r'/api/ocr/template-generation/config/service_manager.ini'
+    },
+}
---
+++ config/expression.py
@@ -0,0 +1,7 @@
+languages_alpha3to2 = {
+    'eng': ['en'],
+    'fra': ['fr'],
+    'nld': ['nl'],
+    'deu': ['de'],
+    'dan': ['da'],
+}
